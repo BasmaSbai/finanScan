d9ae5a642b410800eabbd8432c608d1d6dbcde07
heloufir <essbai.basma@gmail.com>
2024-02-29 18:49:16 -0500

    First commit

---
+++ src/main/java/com/finanScan/finanScan/FinanScanApplication.java
@@ -0,0 +1,13 @@
+package com.finanScan.finanScan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanScanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finanScan/finanScan/FinanScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finanScan.finanScan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
